866ffd7719d6ee5ad32a7f5fbf757b575a0f6a3c
wallace0520 <wallace0520@gmail.com>
2021-10-20 22:32:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,108 @@
+function MostrarModal1() {
+        var display = document.getElementById("modal1").style.display = "none";
+        if(display == "none")
+            document.getElementById("modal1").style.display = 'block';
+        else
+            document.getElementById("modal1").style.display = 'none';
+    }
+
+    function OcultarModal1() {
+        var display = document.getElementById("modal1").style.display;
+        if(display == "block")
+            document.getElementById("modal1").style.display = 'none';
+        else
+            document.getElementById("modal1").style.display = 'block';
+    }
+
+    // FIM MODAL 1
+    
+    
+    function MostrarModal2() {
+        var display = document.getElementById("modal2").style.display = "none";;
+        if(display == "none")
+            document.getElementById("modal2").style.display = 'block';
+        else
+            document.getElementById("modal2").style.display = 'none';
+    }
+
+    function OcultarModal2() {
+        var display = document.getElementById("modal2").style.display;
+        if(display == "block")
+            document.getElementById("modal2").style.display = 'none';
+        else
+            document.getElementById("modal2").style.display = 'block';
+    }
+
+// FIM MODAL 2
+
+function MostrarModal3() {
+    var display = document.getElementById("modal3").style.display = "none";;
+    if(display == "none")
+        document.getElementById("modal3").style.display = 'block';
+    else
+        document.getElementById("modal3").style.display = 'none';
+}
+
+function OcultarModal3() {
+    var display = document.getElementById("modal3").style.display;
+    if(display == "block")
+        document.getElementById("modal3").style.display = 'none';
+    else
+        document.getElementById("modal3").style.display = 'block';
+}
+
+// FIM MODAL 3
+
+function MostrarModal4() {
+    var display = document.getElementById("modal4").style.display = "none";
+    if(display == "none")
+        document.getElementById("modal4").style.display = 'block';
+    else
+        document.getElementById("modal4").style.display = 'none';
+}
+
+function OcultarModal4() {
+    var display = document.getElementById("modal4").style.display ;
+    if(display == "block")
+        document.getElementById("modal4").style.display = 'none';
+    else
+        document.getElementById("modal4").style.display = 'block';
+}
+
+// FIM MODAL 4
+
+function MostrarModal5() {
+    var display = document.getElementById("modal5").style.display = "none";
+    if(display == "none")
+        document.getElementById("modal5").style.display = 'block';
+    else
+        document.getElementById("modal5").style.display = 'none';
+}
+
+function OcultarModal5() {
+    var display = document.getElementById("modal5").style.display;
+    if(display == "block")
+        document.getElementById("modal5").style.display = 'none';
+    else
+        document.getElementById("modal5").style.display = 'block';
+}
+
+// FIM MODAL 5
+
+function MostrarModal6() {
+    var display = document.getElementById("modal6").style.display = "none";
+    if(display == "none")
+        document.getElementById("modal6").style.display = 'block';
+    else
+        document.getElementById("modal6").style.display = 'none';
+}
+
+function OcultarModal6() {
+    var display = document.getElementById("modal6").style.display;
+    if(display == "block")
+        document.getElementById("modal6").style.display = 'none';
+    else
+        document.getElementById("modal6").style.display = 'block';
+}
+
+// FIM MODAL 6
